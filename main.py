18b70fa2aeb0f9396d72b905a53c49e5c98d3fc8
Parimal Mehta <prmehta24@gmail.com>
2024-01-31 01:34:37 +0530

    Task 2 Build an HTTP API to expose function complete.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastkml import kml
 from geopy.distance import geodesic 
+from markupsafe import escape
+from flask import Flask, request
+app = Flask(__name__)
+
+stations=[] #global variable for list of stations.
 
 def parseSEPTAKMLfile():
     with open("./SEPTARegionalRailStations2016.kml", 'rt', encoding="utf-8") as myfile:
@@ -7,20 +12,12 @@ def parseSEPTAKMLfile():
     k = kml.KML()
     k.from_string(doc.encode('utf-8'))
     documentfeatures = list(k.features())
-    #print(documentfeatures)
     folderfeature = list(documentfeatures[0].features())
-    #print(folderfeature)
     placemarkfeatures = list(folderfeature[0].features())
-    print("Number of stations in KML file: ",len(placemarkfeatures))
+    #print("Number of stations in KML file: ",len(placemarkfeatures))
     stations = []
     for placemark in placemarkfeatures:
-        # print(placemarkfeatures[0].name)
         station_name = placemark.extended_data.elements[0]._data[9]['value']
-        # print(placemarkfeatures[0].extended_data.elements[0]._data[9]['value'])
-        # print(placemarkfeatures[1].extended_data.elements[0]._data[9]['value'])
-        # print(placemarkfeatures[2].extended_data.elements[0]._data[9]['value'])
-        # print(placemarkfeatures[3].extended_data.elements[0]._data[9]['value'])
-        # print(placemarkfeatures[0]._geometry.geometry._coordinates)
         # [Longitude, Latitude] given from KML file. Convert to (Lat, Long) for geopy
         coordinates = (placemark._geometry.geometry._coordinates[1], placemark._geometry.geometry._coordinates[0])
         stations.append({"name": station_name, "coordinates": coordinates})
@@ -40,7 +37,7 @@ def formatStationResponse(station):
             "name": station["name"]
         }
     }
-    print("Nearest Station Response as GeoJSON: ",response)
+    #print("Nearest Station Response as GeoJSON: ",response)
     return response
 
 def getClosestStation(currentCoordinates, stations):
@@ -49,15 +46,21 @@ def getClosestStation(currentCoordinates, stations):
     formattedResponse = formatStationResponse(nearest_station)
     return formattedResponse
 
-
-def main():
-    print("In main function")
-    stations = parseSEPTAKMLfile()
-    # for i in range(10):
-    #     print(stations[i])
-    #     print(stations[i]['name'])
-    currentCoords = (39.952925, -75.219457) # coordinates to find closest station.
+@app.route('/closestStation')
+def returnClosestStationResponse():
+    global stations
+    #Handle case where parameters are not given. TODO.
+    lat  = request.args.get('lat', None)
+    long = request.args.get('long', None)
+    currentCoords = (lat, long)
     print("Coords to get closest station for: ",currentCoords)
-    getClosestStation(currentCoords, stations)
+    closestStationResponse = getClosestStation(currentCoords, stations)
+    return closestStationResponse
 
+#Handle default URLs. TODO.
+def main():
+    global stations
+    stations = parseSEPTAKMLfile() #setup stations list for API.
+    print("Stations Loaded. Setup Complete.")
+    app.run(debug=True)
 main()
