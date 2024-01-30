f41cde6dd0e8fc3cf8465db97793d106b64147b0
Parimal Mehta <prmehta24@gmail.com>
2024-01-30 21:13:02 +0530

    Rename document summarising task.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastkml import kml
+from geopy.distance import geodesic 
 
-def parseKMLfile():
+def parseSEPTAKMLfile():
     with open("./SEPTARegionalRailStations2016.kml", 'rt', encoding="utf-8") as myfile:
         doc=myfile.read()
     k = kml.KML()
@@ -11,16 +12,43 @@ def parseKMLfile():
     print(folderfeature)
     placemarkfeatures = list(folderfeature[0].features())
     print(len(placemarkfeatures))
-    print(placemarkfeatures[0].name)
-    print(placemarkfeatures[0].extended_data.elements[0]._data[9]['value'])
-    print(placemarkfeatures[1].extended_data.elements[0]._data[9]['value'])
-    print(placemarkfeatures[2].extended_data.elements[0]._data[9]['value'])
-    print(placemarkfeatures[3].extended_data.elements[0]._data[9]['value'])
+    stations = []
+    for placemark in placemarkfeatures:
+        # print(placemarkfeatures[0].name)
+        station_name = placemark.extended_data.elements[0]._data[9]['value']
+        # print(placemarkfeatures[0].extended_data.elements[0]._data[9]['value'])
+        # print(placemarkfeatures[1].extended_data.elements[0]._data[9]['value'])
+        # print(placemarkfeatures[2].extended_data.elements[0]._data[9]['value'])
+        # print(placemarkfeatures[3].extended_data.elements[0]._data[9]['value'])
+        # print(placemarkfeatures[0]._geometry.geometry._coordinates)
+        # [Longitude, Latitude] given from KML file. Convert to (Lat, Long)
+        coordinates = (placemark._geometry.geometry._coordinates[1], placemark._geometry.geometry._coordinates[0])
+        stations.append({"name": station_name, "coordinates": coordinates})
+    return stations
     
 
-def getTrainDetails():
+def formatStationResponse(station):
     pass
+
+def getClosestStation(currentCoordinates, stations):
+    # closestStation=""
+    # minDistance=-1
+    # for station in stations:
+    #     stationCoords = station.coordinates
+    #     stationDistance = 
+    nearest_station = min(stations, key=lambda station: geodesic(currentCoordinates, station["coordinates"]).miles)
+    print(nearest_station)
+
+    return nearest_station
+
+
 def main():
     print("In main function")
-    parseKMLfile()
+    stations = parseSEPTAKMLfile()
+    # for i in range(10):
+    #     print(stations[i])
+    #     print(stations[i]['name'])
+    currentCoords = (39.952925, -75.219457)
+    getClosestStation(currentCoords, stations)
+
 main()
